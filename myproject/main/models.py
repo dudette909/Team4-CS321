287dd0087614c1b08c06de7862c86a9e6ac8714a
trinity <tahimes4000@gmail.com>
2026-04-01 11:48:37 -0400

    Models.py Classes
    Added two new classes to models.py and imported User from Django's auth library, this way we can track users very easily. Each user should have their own "backpack" that will store "inventoryItems", which will be rewarded upon completing daily puzzles. That has not been added yet.

--- myproject/main/models.py
+++ myproject/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class InventoryItem(models.Model):
+    itemName = models.CharField(max_length = 100)
+    description = models.TextField(blank = True)
+
+    def __str__(self):
+        return self.itemName
+
+class Backpack(models.Model):
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
+    items = models.ManyToManyField(InventoryItem, blank = True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Backpack"
